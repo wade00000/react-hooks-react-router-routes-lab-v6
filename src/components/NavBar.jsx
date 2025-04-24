10a57bc19947eb657bc09ad334ba57c57190d56f
WADE <MEANTOINE12@GMAIL.COM>
2025-04-24 16:47:40 +0300

    complete

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,21 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+    <NavLink to='/'>
+      Home 
+    </NavLink>
+
+    <NavLink to='/actors'>
+      Actors
+    </NavLink>
+
+    <NavLink to='/directors'>
+      Directors
+    </NavLink>
+
+    <NavLink to='/movie/:id'>
+      Movie
+    </NavLink>
     </nav>
     );
 };
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,34 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import NavBar from "../components/NavBar";
 
 function Movie() {
+const [user, setUser] = useState({});
+const params = useParams();
+const userId = params.id;
+
+
+  useEffect(()=>{
+    fetch(`http://localhost:4000/movies/${userId}`)
+    .then(r=> r.json())
+    .then(data => setUser(data))
+    .catch(error=>console.error(error))
+  },[userId])
+  
+  if(!user.title){
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{user.title}</h1>
+        <p>Duration : {user.time} minutes</p>
+        <h3>Genre : {user.genres.join(', ')}</h3>
       </main>
     </>
   );
